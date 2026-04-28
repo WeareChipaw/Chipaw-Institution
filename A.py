662c33059dfd5ff83d91d12174b91db6a21c1dd5
Engr. CJ <mainit.christianjay27@gmail.com>
2026-04-28 05:03:29 +0000

    #unfinished

--- A.py
+++ A.py
@@ -1,18 +1,41 @@
-try:
-    with open("Shaolin.txt", "x") as foyl:
-        print("File Created succesfully!")
-except:
-    print("File already exist")
+while True:
+    print("1. Create")
+    print("2. Append")
+    print("3. Read")
+    print("4. Update")
+    print("5. Delete")
+    print("6. Exit)
+    try:
+        choice = int(input("Enter choice: "))
+    except ValueError:
+        print("Enter numbers only")
 
-with open("Shaolin.txt", "w") as foyl:
-    foyl.write("Wassap mga ka wonders!")
-
-with open("Shaolin.txt", "a") as foyl:
-    foyl.write("\nDon't go there, mannnn.")
-
-with open("Shaolin.txt", "r") as foyl:
-    lines = foyl.readlines()
-    print(lines)
+    match choice:
+        case 1:
+            try:
+                filename = input("Enter the filename (example: test.txt): ")
+                with open(filename, "x") as file:
+                    texts = input("Write contents: ")
+                    file.write(texts)
+                print("File Created succesfully")
+            except:
+                print("File already exist")
+        case 2:
+            with open (filename, "a") as file:
+                contents = input("Write contents: ")
+                file.write(contents)
+            print("Added contents successfuly!")
+        case 3:
+             with open(filename, "r") as file:
+                contents = file.readlines()
+                print("\n",contents)
+        case 4:
+            with open(filename, "r") as file:
+                
+        case 5:
+            print("Delete")
+        case 6:
+            break
 
 
 
